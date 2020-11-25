501876eb0942a28601948c2d266fc0df1b67a3f9
mean <vaibhavbisht78@gmail.com>
2020-11-25 19:16:20 +0530

    commit on 'main'

--- app.js
+++ app.js
@@ -11,8 +11,16 @@ app.listen(port);
 
 app.get('/', (req, res) => {
     res.sendFile('./views/index.html', { root: __dirname });
-})
+});
 
 app.get('/about', (req, res) => {
     res.sendFile('./views/about.html', { root: __dirname });
-})
+});
+
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+app.use((req,res) => {
+    res.status(404).sendFile('views/404.html', { root: __dirname })
+});
