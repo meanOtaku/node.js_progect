69094c940a59d390b36b6ee9fb0b5f546e8ffd0a
mean <vaibhavbisht78@gmail.com>
2020-11-30 18:46:36 +0530

    commit on 'main'

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blogs');
-const { response } = require('express');
-const { render } = require('ejs');
+const blogRoutes = require('./routes/blogRoutes');
 
 const dbURI = 'mongodb+srv://honeybisht123:honeybisht123@nodepro.a6gzy.mongodb.net/node-pro?retryWrites=true&w=majority'
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true } )
@@ -38,56 +36,7 @@ app.get('/about', (req, res) => {
 });
 
 //blog route
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt : -1 })
-    .then((result) => {
-        res.render('index', { title: 'All Blogs', blogs: result });
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-});
-
-
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-  });
-
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-    .then((result) => {
-        res.redirect('/blogs');
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-});
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-    .then(result => {
-        res.render('details', {blog: result, title: 'Blog Details'});
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-});
-
-app.delete('/blogs/:id', (req,res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-    .then((result) => {
-        res.json({ redirect: '/blogs' });
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-})
+app.use('/blogs', blogRoutes);
 
 
 app.use((req,res) => {
